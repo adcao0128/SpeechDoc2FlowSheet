9fa90191e8a3ee279fd27ae44c35a2cafaba44d9
adcao0128 <adc135@case.edu>
2023-04-06 18:01:23 -0400

    final2

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const path = require('path');
+module.exports = {
+  outputDir: path.resolve(__dirname, '../server/public')
+};
